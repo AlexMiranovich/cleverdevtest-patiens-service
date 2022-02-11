aba8c12d0f58878d15331de208ecb68b4b039e55
Alex Miranovich <mir92on@gmail.com>
2022-02-11 13:21:24 +0300

    init commit

---
+++ src/main/java/com/cleverdev/patientservice/CleverdevtestPatiensServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cleverdev.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleverdevtestPatiensServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleverdevtestPatiensServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleverdev/patientservice/CleverdevtestPatiensServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleverdev.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverdevtestPatiensServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
